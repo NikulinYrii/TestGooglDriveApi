caf6ff599e8448d2b1e5eb3a6d3905dfaf4b1d0b
Юрій Нікулін <asefcvj@gmail.com>
2020-03-29 18:05:46 +0300

    add upload file

--- GoogleDriveApp/Program.cs
+++ GoogleDriveApp/Program.cs
@@ -6,10 +6,10 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Google.Apis.Auth.OAuth2;
-using Google.Apis.Drive.v2;
+using Google.Apis.Drive.v3;
 using Google.Apis.Services;
 using Google.Apis.Util.Store;
-using File = Google.Apis.Drive.v2.Data.File;
+using File = Google.Apis.Drive.v3.Data.File;
 
 namespace GoogleDriveApp
 {
@@ -17,16 +17,21 @@ namespace GoogleDriveApp
     {
         private static string[] scope = { DriveService.Scope.Drive };
         private static string aplicationName = "Quickstart";
+        private static string folderId = "1jrWZPEewWJ3rtvWHGLf0H6dKMpUHLg3C";
+
         static void Main(string[] args)
         {
             UserCredential credential = GetUserCredential();
             DriveService service = GetDriveService(credential);
 
-            IList<File> files = service.Files.List().Execute().Items;
-            foreach(var file in files)
-            {
-                Console.WriteLine("File title: {0}, id {1}", file.Title, file.Id);
-            }
+            /* IList<File> files = service.Files.List().Execute().Items;
+             foreach(var file in files)
+             {
+                 Console.WriteLine("File title: {0}, id {1}", file.Title, file.Id);
+             }*/
+
+            UploadFileToDrive(service, "test", @"C:\Users\y.nikulin\Desktop\standart_stoika v24012020.pdf", "application/pdf");
+
             Console.ReadLine();
         }
 
@@ -54,6 +59,25 @@ namespace GoogleDriveApp
                     HttpClientInitializer = credential,
                     ApplicationName = aplicationName
                 });
+        }
+
+        private static string UploadFileToDrive(DriveService service, string fileName, string filePath, string contentType)
+        {
+            var fileMetadata = new File();
+            fileMetadata.Name = fileName;
+            fileMetadata.Parents = new List<string> { folderId };
+
+            FilesResource.CreateMediaUpload request;
+
+            using (var stream = new FileStream(filePath, FileMode.Open))
+            {
+                request = service.Files.Create(fileMetadata, stream, contentType);
+                request.Upload();
+            }
+
+            var file = request.ResponseBody;
+
+            return file.Id;
         }
     }
 }
