2be1b2b3626d17850e20f7c351e392e4e3ab94fc
Юрій Нікулін <asefcvj@gmail.com>
2020-03-29 19:18:27 +0300

    add download file from drive

--- GoogleDriveApp/Program.cs
+++ GoogleDriveApp/Program.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Google.Apis.Auth.OAuth2;
+using Google.Apis.Download;
 using Google.Apis.Drive.v3;
 using Google.Apis.Services;
 using Google.Apis.Util.Store;
@@ -24,13 +25,22 @@ namespace GoogleDriveApp
             UserCredential credential = GetUserCredential();
             DriveService service = GetDriveService(credential);
 
-            /* IList<File> files = service.Files.List().Execute().Items;
-             foreach(var file in files)
-             {
-                 Console.WriteLine("File title: {0}, id {1}", file.Title, file.Id);
-             }*/
+            var files = GetFiles(service);
+            foreach (var file in files)
+            {
+                string saveFile = file.Name + "." + file.MimeType.Split('/')[1];
+                Console.WriteLine("File title: {0}, id {1}", file.Name, file.Id);
+                Console.WriteLine(saveFile);
+                Console.WriteLine("----------------------------------------------------");
+                if (file.MimeType.Split('/')[1] == "pdf")
+                {
+                    DownloadFileFromDrive(service, file.Id, @"D:\source\"+saveFile);
+                }
+            }
 
-            UploadFileToDrive(service, "test", @"C:\Users\y.nikulin\Desktop\standart_stoika v24012020.pdf", "application/pdf");
+            //UploadFileToDrive(service, "test", @"C:\Users\y.nikulin\Desktop\standart_stoika v24012020.pdf", "application/pdf");
+
+            //DownloadFileFromDrive(service, "1xrdjU36ZXF0irvq4Byr4TPi0ugZT9Gmk", @"D:\source\");
 
             Console.ReadLine();
         }
@@ -61,6 +71,11 @@ namespace GoogleDriveApp
                 });
         }
 
+        private static IList<File> GetFiles(DriveService service)
+        {
+            return service.Files.List().Execute().Files;
+        }
+
         private static string UploadFileToDrive(DriveService service, string fileName, string filePath, string contentType)
         {
             var fileMetadata = new File();
@@ -78,6 +93,37 @@ namespace GoogleDriveApp
             var file = request.ResponseBody;
 
             return file.Id;
+        }
+
+        private static void DownloadFileFromDrive(DriveService service, string fileId, string filePath)
+        {
+            var request = service.Files.Get(fileId);
+
+            using(var memoryStream = new MemoryStream())
+            {
+                request.MediaDownloader.ProgressChanged += (IDownloadProgress progress) =>
+                {
+                    switch (progress.Status)
+                    {
+                        case DownloadStatus.Downloading:
+                            Console.WriteLine(progress.BytesDownloaded);
+                            break;
+                        case DownloadStatus.Completed:
+                            Console.WriteLine("Download Complete");
+                            break;
+                        case DownloadStatus.Failed:
+                            Console.WriteLine("Download Failed");
+                            break;
+                    }
+                };
+
+                request.Download(memoryStream);
+
+                using(var fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+                {
+                    fileStream.Write(memoryStream.GetBuffer(), 0, memoryStream.GetBuffer().Length);
+                }
+            }
         }
     }
 }
